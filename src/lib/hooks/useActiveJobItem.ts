540e3022167ae7f90151815580fef91f0c4358d7
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2024-07-15 18:12:59 +0600

    fetch data using tanstack query for appropriate caching

--- src/lib/hooks/useActiveJobItem.ts
+++ src/lib/hooks/useActiveJobItem.ts
@@ -1,34 +1,59 @@
-import { useEffect, useState } from "react";
-import useActiveJobId from "./useActiveJobId";
 import { JobItemDetailsType } from "../types";
+import useActiveJobId from "./useActiveJobId";
+import { useQuery } from "@tanstack/react-query";
 
 function useActiveJobItem() {
-  const [activeJobItem, setActiveJobItem] = useState<JobItemDetailsType | null>(
-    null
-  );
-
-  const [isItemLoading, setIsItemLoading] = useState(false);
   const activeJobId = useActiveJobId();
 
-  useEffect(() => {
-    if (!activeJobId) return;
-
-    const handleActiveJobFetch = async () => {
-      setIsItemLoading(true);
+  const { data, isLoading } = useQuery({
+    queryKey: ["job-item", activeJobId],
+    queryFn: async (): Promise<JobItemDetailsType> => {
       const response = await fetch(
         `http://localhost:3000/jobDetails/${activeJobId}`
       );
 
       const data = await response.json();
+      return data;
+    },
+    staleTime: 1000 * 60 * 60,
+    refetchOnWindowFocus: false,
+    retry: false,
+    enabled: !!activeJobId,
+  });
 
-      setActiveJobItem(data);
-      setIsItemLoading(false);
-    };
+  const activeJobItem = data;
+  const isItemLoading = isLoading;
 
-    handleActiveJobFetch();
-  }, [activeJobId]);
-
-  return { isItemLoading, activeJobItem } as const;
+  return { activeJobItem, isItemLoading };
 }
+
+// function useActiveJobItem() {
+//   const [activeJobItem, setActiveJobItem] = useState<JobItemDetailsType | null>(
+//     null
+//   );
+
+//   const [isItemLoading, setIsItemLoading] = useState(false);
+//   const activeJobId = useActiveJobId();
+
+//   useEffect(() => {
+//     if (!activeJobId) return;
+
+//     const handleActiveJobFetch = async () => {
+//       setIsItemLoading(true);
+//       const response = await fetch(
+//         `http://localhost:3000/jobDetails/${activeJobId}`
+//       );
+
+//       const data = await response.json();
+
+//       setActiveJobItem(data);
+//       setIsItemLoading(false);
+//     };
+
+//     handleActiveJobFetch();
+//   }, [activeJobId]);
+
+//   return { isItemLoading, activeJobItem } as const;
+// }
 
 export default useActiveJobItem;
