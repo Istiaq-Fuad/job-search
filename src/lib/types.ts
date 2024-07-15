9c674a086a309ad6a1e90d93312475d38678fa99
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2024-07-15 12:48:32 +0600

    declare the types

---
+++ src/lib/types.ts
@@ -0,0 +1,19 @@
+export type JobItemType = {
+  id: number;
+  title: string;
+  badgeLetters: string;
+  company: string;
+  relevanceScore: number;
+  daysAgo: number;
+};
+
+export type JobItemDetailsType = JobItemType & {
+  description: string;
+  qualifications: string[];
+  reviews: string[];
+  duration: string;
+  salary: string;
+  location: string;
+  coverImgURL: string;
+  companyURL: string;
+};
